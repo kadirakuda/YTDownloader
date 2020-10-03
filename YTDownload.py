e2f9c849eb7cabb550996cdfece1a8e22e490bdb
kudakadira <kadirakuda@gmail.com>
2020-10-03 18:47:44 +0100

    added option for video or audio file

--- YTDownload.py
+++ YTDownload.py
@@ -1,5 +1,34 @@
 #get link from user
-#convert the video to mp3
-#download the mp3
+#convert the video to mp4 audio
+#download the mp4
 
-video_link = input("Video link: ")
+from pytube import YouTube
+
+def download_audio(video_link):
+    #filter the available streams to audio only
+    yt = YouTube(video_link)
+    yt.streams.filter(only_audio=True).first().download()
+    print("\n Your audio file is ready!")
+
+def download_video(video_link):
+    #filter the available streams to audio only
+    yt = get_ytlink()
+    yt.streams.first().download()
+    print("\n Your video file is ready!")
+
+while True:
+    print("\n")
+    print("Welcome to YT Downloader \n")
+    print("Selecter an option below: ")
+    format = input("Enter 'V' for video, 'A' for audio only download or 'X' to exit: \n")
+    format.lower()
+    if format == "v":
+        #User link to download youtube video
+        video_link = input("enter link: ")
+        download_video(video_link)
+    elif format == "a":
+        #User link to download youtube video
+        video_link = input("enter link: ")
+        download_audio(video_link)
+    elif format == "x":
+        break
