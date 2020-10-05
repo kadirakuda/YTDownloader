08fb03eae406278920852a069713000d58a71da2
kudakadira <kadirakuda@gmail.com>
2020-10-05 18:02:22 +0100

    added ability to select video quality - High, Low or Medium

--- YTDownload.py
+++ YTDownload.py
@@ -10,25 +10,35 @@ def download_audio(video_link):
     yt.streams.filter(only_audio=True).first().download()
     print("\n Your audio file is ready!")
 
-def download_video(video_link):
-    #filter the available streams to audio only
-    yt = get_ytlink()
-    yt.streams.first().download()
-    print("\n Your video file is ready!")
+def download_video(video_link, quality):
+    yt = YouTube(video_link)
+    if quality == 'h':
+        #selects the first stream which is always the hightest quality stream
+        yt.streams.first().download()
+        print("\n Your video file is ready!")
+    elif quality == 'l':
+        #use filter to select a low resolution as per the user request
+        yt.streams.filter(res="240p").first().download() #
+        print("\n Your video file is ready!")
+    elif quality == 'm':
+        #use filter to select a medium resolution as per the user request
+        yt.streams.filter(res="360p").first().download()
+
 
 while True:
     print("\n")
     print("Welcome to YT Downloader \n")
     print("Selecter an option below: ")
     format = input("Enter 'V' for video, 'A' for audio only download or 'X' to exit: \n")
-    format.lower()
-    if format == "v":
+    if format.lower() == "v":
         #User link to download youtube video
         video_link = input("enter link: ")
-        download_video(video_link)
-    elif format == "a":
+        quality = input("Enter a letter to choose video quality 'M - medium', 'H - High' or 'L - low':")
+        download_video(video_link, quality.lower())
+
+    elif format.lower() == "a":
         #User link to download youtube video
         video_link = input("enter link: ")
         download_audio(video_link)
-    elif format == "x":
+    elif format.lower() == "x":
         break
